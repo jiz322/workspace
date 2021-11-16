1ed0d3dde4b3992e2af75124b77dee66800ef06f
jiz322 <jiz322@lehigh.edu>
2021-11-15 20:13:36 -0500

    sig

--- project3_serial.cpp
+++ project3_serial.cpp
@@ -16,6 +16,9 @@ class HashTable
         std::vector<T> values1;
         std::vector<T> values2;
         int sizeOfTable;
+        int HASH1 = 2147483647;
+        int HASH2 = 479001599;
+        int START = 1; //for populate table
 
         HashTable (int sizeOfTable)
         {
@@ -31,8 +34,8 @@ class HashTable
 
         bool contains (T x)
         {
-            int hash1 = hash(x, 2147483647);
-            int hash2 = hash(x, 479001599);
+            int hash1 = hash(x, HASH1);
+            int hash2 = hash(x, HASH2);
             if (values1.at(hash1) == x)
             {
                 return true;
@@ -44,13 +47,6 @@ class HashTable
             return false;
         }
 
-        //double the sizeOfTable of both table
-        void resize ()
-        {
-            sizeOfTable = 2*sizeOfTable;
-            values1.resize(sizeOfTable*2, NULL);
-            values2.resize(sizeOfTable*2, NULL);
-        }
 
         //swap value
         T swap(T x, int i, int table)
@@ -71,6 +67,34 @@ class HashTable
             }
         }
 
+        //double the sizeOfTable of both table
+        //have to compute 
+        void resize ()
+        {
+            int previousSize = sizeOfTable;
+            sizeOfTable = 2*sizeOfTable;
+            values1.resize(sizeOfTable*2, NULL);
+            values2.resize(sizeOfTable*2, NULL);
+            //Waste compute power here since half of table is NULL
+            for (typename std::vector<T>::iterator it = values1.begin() ; it != values1.end(); ++it)
+            {
+                if (*it != NULL){
+                    //erase this and call add
+                    T x = *it;
+                    values1.erase(it);
+                    add(x);
+                }
+            }
+            for (typename std::vector<T>::iterator it = values2.begin() ; it != values2.end(); ++it)
+            {
+                if (*it != NULL){
+                    T x = *it;
+                    values2.erase(it);
+                    add(x);
+                }
+            }
+        }
+
         bool add (T x)
         {
             if (contains(x))
@@ -78,8 +102,8 @@ class HashTable
                 return false;
             }
             int LIMIT =  sizeOfTable/2;
-            int hash1 = hash(x, 2147483647); //suprisely, this is a prime number
-            int hash2 = hash(x, 479001599); // another prime number
+            int hash1 = hash(x, HASH1); //suprisely, this is a prime number
+            int hash2 = hash(x, HASH2); // another prime number
             int tableToInsert = hash1%2+1; //1 or 2, randomly select the first table to insert
             for (int i = 0; i < LIMIT; i++)
             {
@@ -110,8 +134,8 @@ class HashTable
 
         bool remove(T x)
         {
-            int hash1 = hash(x, 2147483647); 
-            int hash2 = hash(x, 479001599);
+            int hash1 = hash(x, HASH1); 
+            int hash2 = hash(x, HASH2);
             if (values1.at(hash1) == x)
             {
                 swap(NULL, hash1, 1);
@@ -147,10 +171,12 @@ class HashTable
         }
 
         //unsafe -- only works for int hashtable
+        //unsafe -- 0 is not allowed
+        //ToDO: change hash function
         //populate integers from 1 to size
         void populate(int size)
         {
-            for (int i = 0; i < size; i++){
+            for (int i = START; i < size+START; i++){
                 add(i);
             }
         }
@@ -161,38 +187,43 @@ class HashTable
 int main(int argc, char** argv) 
 {
     HashTable <int> a = HashTable<int>(5);
-    a.add(3);
-    a.add(4);
-    a.add(5);
-    a.add(6);
-    a.add(7);
-    //a.add(8);
-   // a.add(9);
-    printf("%d", a.sizeOfTable);
+//     a.add(3);
+//     a.add(4);
+//     a.add(5);
+//     a.add(6);
+//     a.add(7);
+//     //a.add(8);
+//    // a.add(9);
+//     printf("%d", a.sizeOfTable);
 
+//     printf ("%d", a.values1[0]);
+//     printf ("%d", a.values1[1]);
+//     printf ("%d", a.values1[2]);
+//     printf ("%d", a.values1[3]);
+//     printf ("%d", a.values1[4]);
+//     printf ("%d", a.values2[0]);
+//     printf ("%d", a.values2[1]);
+//     printf ("%d", a.values2[2]);
+//     printf ("%d", a.values2[3]);
+//     printf ("%d", a.values2[4]);
+
+//     printf ("%d", a.contains(4));
+//     printf ("%d", a.contains(3));
+//     printf ("%d", a.contains(5));
+//     printf ("size%d", a.size());
+//     printf ("%d", a.remove(4));
+//     printf ("%d", a.remove(3));
+//     printf ("%d", a.remove(5));
+//     printf ("%d", a.contains(4));
+//     printf ("%d", a.contains(3));
+//     printf ("%d", a.contains(5));
+//     printf ("size%d", a.size());
+    a.populate(10);
     printf ("%d", a.values1[0]);
     printf ("%d", a.values1[1]);
     printf ("%d", a.values1[2]);
     printf ("%d", a.values1[3]);
-    printf ("%d", a.values1[4]);
-    printf ("%d", a.values2[0]);
-    printf ("%d", a.values2[1]);
-    printf ("%d", a.values2[2]);
-    printf ("%d", a.values2[3]);
-    printf ("%d", a.values2[4]);
-
-    printf ("%d", a.contains(4));
     printf ("%d", a.contains(3));
-    printf ("%d", a.contains(5));
-    printf ("size%d", a.size());
-    printf ("%d", a.remove(4));
-    printf ("%d", a.remove(3));
-    printf ("%d", a.remove(5));
-    printf ("%d", a.contains(4));
-    printf ("%d", a.contains(3));
-    printf ("%d", a.contains(5));
-    printf ("size%d", a.size());
-    a.populate(10);
     printf ("size%d", a.size());
 
 }
