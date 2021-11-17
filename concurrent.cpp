7702dd8491c8575e5bbb7d7a59c23cecfae106a1
jiz322 <jiz322@lehigh.edu>
2021-11-17 11:44:19 -0500

    c

--- concurrent.cpp
+++ concurrent.cpp
@@ -46,8 +46,8 @@ class HashTable
             {
                 //l1[i] = std::mutex;
                 //l2[i] = std::mutex;
-                std::mutex mtx;
-                *(l1+i) = &mtx;
+                std::mutex *mtx;
+                *(l1+i) = *mtx;
                 //mutexes1.push_back( mtx));
                 // mutexes2.push_back( std::mutex mutex2);
             }
