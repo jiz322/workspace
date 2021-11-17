de7f940825a12cc9b160d34a0c6cbf3a90dcb3c4
jiz322 <jiz322@lehigh.edu>
2021-11-17 09:00:41 -0500

    f

--- concurrent.cpp
+++ concurrent.cpp
@@ -31,8 +31,8 @@ class HashTable
         int HASH2 = 479001599;
         int START = 100000000; //for populate table
         int NUM_LOCKS = 30;
-        vector<std::shared_mutex mutex_> mutexes1(10);
-        vector<std::shared_mutex mutex_> mutexes2(10);
+        vector<std::mutex> mutexes(NUM_LOCKS);
+        vector<std::mutex> mutexes(NUM_LOCKS);
         HashTable (int sizeOfTable)
         {
             this->sizeOfTable = sizeOfTable;
