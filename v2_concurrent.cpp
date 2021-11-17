bcd31eadb5211a25656b7fce632af981abf645d0
jiz322 <jiz322@lehigh.edu>
2021-11-17 17:02:52 -0500

    s

--- v2_concurrent.cpp
+++ v2_concurrent.cpp
@@ -264,7 +264,7 @@ class HashTable
 int main(int argc, char** argv) 
 {
     //Expected final size is about populated + (WORKS -  populated)/10
-    int WORKS = 1000;
+    int WORKS = 10000;
     int POPULATE = 10000;
     int NUM_THREAD = 8;
     int STEP = WORKS/NUM_THREAD;
