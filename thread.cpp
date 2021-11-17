ebf9aabfc7e9c8d6ebdfd1ff2283bdb233ed3826
jiz322 <jiz322@lehigh.edu>
2021-11-17 06:02:00 -0500

    G

--- thread.cpp
+++ thread.cpp
@@ -1,17 +1,25 @@
-
-#include <iostream>
 #include <stdio.h>
 #include <random>
+#include <thread>
+#include <mutex>
+#include <shared_mutex> 
+#include <chrono>
+#include <future>
+#include <unistd.h>
+#include <iostream>
+#include <ctime>
 #include <vector>
+#include <cassert>
+#include <cstdlib>
+#include <string>
 #include <stdlib.h>
-#include <stdio.h>
 int main(int argc, char** argv){
     auto do_work = [&](){
         printf("aaa");
     };
-    std::vector<std::crend> threads;
+    std::vector<std::thread> threads;
     for (int i = 0; i < 4; i++){
-        threads.push_back(std::crend(do_work));
+        threads.push_back(std::thread(do_work));
     }
     for (auto &th : threads){
         th.join();
