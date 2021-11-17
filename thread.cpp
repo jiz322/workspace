b03a1fb82824584da902fde6107708bd206a372f
jiz322 <jiz322@lehigh.edu>
2021-11-17 05:47:31 -0500

    x

--- thread.cpp
+++ thread.cpp
@@ -2,16 +2,19 @@
 #include <iostream>
 #include <stdio.h>
 #include <random>
+#include <vector>
+#include <stdlib.h>
+#include <stdio.h>
 int main(int argc, char** argv){
     auto do_work = [&](){
         printf("aaa");
     };
     std::vector<std::thread> threads;
-            for (int i = 0; i < 4; i++){
-                threads.push_back(std::thread(do_work));
-            }
-            for (auto &th : threads){
-                th.join();
-                printf("\nThe balance after join is";
+    for (int i = 0; i < 4; i++){
+        threads.push_back(std::thread(do_work));
+    }
+    for (auto &th : threads){
+        th.join();
+        printf("\nThe balance after join is";
     }
 }
