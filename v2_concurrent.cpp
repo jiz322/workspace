69b75f84845f9af128dfcc578577a96bcd773f18
jiz322 <jiz322@lehigh.edu>
2021-11-17 18:48:05 -0500

    e

--- v2_concurrent.cpp
+++ v2_concurrent.cpp
@@ -144,7 +144,7 @@ class HashTable
         {
             std::unique_lock<std::shared_timed_mutex> lock (mtx_resize);
             int previousSize = sizeOfTable;
-            sizeOfTable = sizeOfTable*3;
+            sizeOfTable = sizeOfTable*5;
             vector<T> values1_old = values1;
             vector<T> values2_old = values2;
             values1.assign(sizeOfTable, NULL);
