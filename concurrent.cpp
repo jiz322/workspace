0c47fc7d45b81ac46ce48d23dc05485f05795a86
jiz322 <jiz322@lehigh.edu>
2021-11-17 06:31:41 -0500

    c

--- concurrent.cpp
+++ concurrent.cpp
@@ -261,7 +261,27 @@ int main(int argc, char** argv)
 
     //lmd for calling do works
     auto lmd = [&](HashTable<int> h, int begin, int work){
-        doWork(a, begin, work);
+        for (int i = 0; i < work; i++){
+            //10% add
+            if (i % 10 ==9)
+            {
+                h.add(2*begin+i);
+            }
+            //10%remove
+            //Unfair to this concurrent version,
+            //Since thread are not balance loaded
+            //first thread will remove succesfully whereas the rest can not.
+            else if (i % 10 == 1)
+            {
+                h.remove(begin+i);
+            }
+            //80% contains, always true in this serial one
+            else
+            {
+                int c = h.contains(begin+i);
+                //printf("\n%d",c);
+            }
+        }
     };   
     vector<std::thread> threads;
     for (int i = 0; i < NUM_THREAD; i++){
