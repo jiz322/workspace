d4b8e17f48c10e392f033863d24d3508ce81ea0f
jiz322 <jiz322@lehigh.edu>
2021-11-17 20:11:01 -0500

    s

--- try.cpp
+++ try.cpp
@@ -1,6 +1,9 @@
-__transaction_relaxed {
-   x = 1;
-  while (x != 0) {}
+int main(int argc, char** argv) 
+{
+    __transaction_relaxed {
+    x = 1;
+    while (x != 0) {}
+    }
+    while (x  != 1) {}
+    x = 0;
 }
-while (x  != 1) {}
-x = 0;
