024da3dbdcbf77bed4eb21c252cba86f35973b02
jiz322 <jiz322@lehigh.edu>
2021-11-17 20:21:19 -0500

    s

--- try.cpp
+++ try.cpp
@@ -1,11 +1,33 @@
 #include <atomic>
+#include <iostream>       // std::cout
+#include <thread>
+void foo(std::atomic<int> x)
+{
+    __transaction_relaxed 
+    {
+        x = 1;
+        while (x != 0) {}
+    }
+}
+void bar(std::atomic<int> x)
+{
+    while (x  != 1) {}
+    x = 0;
+}
 int main(int argc, char** argv) 
 {
     std::atomic<int> x = 0;
-    __transaction_relaxed {
-    x = 1;
-    while (x != 0) {}
-    }
-    while (x  != 1) {}
-    x = 0;
+    
+    std::thread first (foo, x);     // spawn new thread that calls foo()
+    std::thread second (bar,x);
+
+    std::cout << "main, foo and bar now execute concurrently...\n";
+
+        // synchronize threads:
+    first.join();                // pauses until first finishes
+    second.join();               // pauses until second finishes
+
+    std::cout << "foo and bar completed.\n";
+
+    return 0;
 }
